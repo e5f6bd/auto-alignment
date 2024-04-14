5809b4a489b57b18c1ea10200eb042b4e3b0c41c
e5f6bd <41416412+e5f6bd@users.noreply.github.com>
2024-04-14 23:56:44 +0900

    basic event loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use sdl2::{event::Event, pixels::Color};
+
+fn main() -> anyhow::Result<()> {
+    let sdl = sdl2::init()?;
+    let video = sdl.video()?;
+    let mut event = sdl.event_pump()?;
+
+    let window = video
+        .window("PAM controller", 960, 540)
+        .allow_highdpi()
+        .build()?;
+    let mut canvas = window.into_canvas().present_vsync().build()?;
+
+    'outer_loop: loop {
+        for event in event.poll_iter() {
+            if let Event::Quit { .. } = event {
+                break 'outer_loop;
+            }
+        }
+
+        canvas.set_draw_color(Color::BLACK);
+        canvas.clear();
+        canvas.present();
+    }
+
+    Ok(())
 }
