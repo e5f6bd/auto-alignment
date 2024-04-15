d6595d6ab2f674b67acacd84b594aa93f3ea2451
e5f6bd <41416412+e5f6bd@users.noreply.github.com>
2024-04-15 23:46:07 +0900

    Visualize joystick movement

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,46 @@
-use sdl2::{event::Event, pixels::Color};
+use sdl2::{event::Event, pixels::Color, rect::Rect};
 
 fn main() -> anyhow::Result<()> {
     let sdl = sdl2::init()?;
-    let video = sdl.video()?;
-    let mut event = sdl.event_pump()?;
 
-    let window = video
-        .window("PAM controller", 960, 540)
-        .allow_highdpi()
-        .build()?;
+    let w = 960;
+    let h = 540;
+
+    let video = sdl.video()?;
+    let window = video.window("PAM controller", w, h).build()?;
     let mut canvas = window.into_canvas().present_vsync().build()?;
+
+    let joystick = sdl.joystick()?;
+    let _joystick = joystick.open(0)?;
+    let mut joystick_position = [0; 4];
+
+    let mut event = sdl.event_pump()?;
 
     'outer_loop: loop {
         for event in event.poll_iter() {
-            if let Event::Quit { .. } = event {
-                break 'outer_loop;
+            match event {
+                Event::JoyAxisMotion {
+                    axis_idx, value, ..
+                } => {
+                    joystick_position[axis_idx as usize] = dbg!(value) as i32;
+                }
+                Event::Quit { .. } => break 'outer_loop,
+                _ => (),
             }
         }
 
         canvas.set_draw_color(Color::BLACK);
         canvas.clear();
+
+        for i in 0..2 {
+            let scale = 256;
+            let x = w as i32 * (2 * i + 1) / 4 + joystick_position[2 * i as usize] / scale;
+            let y = h as i32 / 2 + joystick_position[2 * i as usize + 1] / scale;
+            let s = 20;
+            canvas.set_draw_color(Color::RED);
+            canvas.draw_rect(Rect::new(x - s / 2, y - s / 2, s as u32, s as u32))?;
+        }
+
         canvas.present();
     }
 
