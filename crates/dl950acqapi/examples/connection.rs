f4a76f0443736246838e16b9c061316f7262e047
e5f6bd <41416412+e5f6bd@users.noreply.github.com>
2024-06-03 08:20:50 +0900

    DL950: Data retrieval

--- crates/dl950acqapi/examples/connection.rs
+++ crates/dl950acqapi/examples/connection.rs
@@ -1,7 +1,7 @@
-use std::net::IpAddr;
+use std::{net::IpAddr, thread::sleep, time::Duration};
 
 use clap::Parser;
-use dl950acqapi::{Api, WireType::Vxi11};
+use dl950acqapi::{Api, ChannelNumber, WireType::Vxi11};
 
 #[derive(Parser)]
 struct Opts {
@@ -13,6 +13,43 @@ fn main() -> anyhow::Result<()> {
 
     let api = Api::init()?;
     let handle = api.open_trigger_asnyc(Vxi11, &opts.ip_address.to_string())?;
-    println!("Device connected!");
+
+    let channel = ChannelNumber {
+        channel: 5,
+        sub_channel: 1,
+    };
+    let mut current_count = 0;
+
+    handle.start()?;
+    for loop_index in 0..100 {
+        handle.latch_data()?;
+        let next_count = handle.latched_acquisition_count()?;
+        for i in current_count + 1..=next_count {
+            handle.set_acquisition_index(i)?;
+            let length = handle.triggered_samples_len(channel)?;
+            loop {
+                let expected_len = length * 2;
+                let mut buffer = vec![0; expected_len * 2];
+                let result = handle.read_triggered_waveform(channel, &mut buffer)?;
+                println!("Acquisition {i}: expected length = {expected_len}, return = {result:?}");
+                println!("{:?}...", &buffer[..100.min(result.received_len)]);
+                if result.completed {
+                    break;
+                }
+            }
+        }
+        current_count = next_count;
+        println!(
+            "Loop {loop_index}: Current acquisition count so far: {}",
+            next_count
+        );
+        if next_count >= 1 {
+            break;
+        }
+        sleep(Duration::from_millis(1));
+    }
+    handle.stop()?;
+
+    println!("Done!");
     Ok(())
 }
--- crates/dl950acqapi/src/lib.rs
+++ crates/dl950acqapi/src/lib.rs
@@ -1,8 +1,14 @@
-use std::ffi::{CString, NulError};
+use std::{
+    ffi::{CString, NulError},
+    marker::PhantomData,
+};
 
+use chrono::{DateTime, NaiveDateTime};
+use connection_mode::{ConnectionMode, FreeRun, TriggerAsync, TriggerMode, TriggerSync};
 use dl950acqapi_sys::{
-    ScCloseInstrument, ScExit, ScInit, ScOpenInstrument, SC_ERROR, SC_FREERUN, SC_SUCCESS,
-    SC_TRIGGER, SC_TRIGGER_ASYNC, SC_WIRE_HISLIP, SC_WIRE_USBTMC, SC_WIRE_VISAUSB, SC_WIRE_VXI11,
+    ScCloseInstrument, ScExit, ScGetAcqCount, ScGetAcqData, ScGetAcqDataLength, ScGetLatchAcqCount,
+    ScInit, ScLatchData, ScOpenInstrument, ScSetAcqCount, ScStart, ScStop, SC_ERROR, SC_SUCCESS,
+    SC_WIRE_HISLIP, SC_WIRE_USBTMC, SC_WIRE_VISAUSB, SC_WIRE_VXI11,
 };
 use thiserror::Error;
 
@@ -67,16 +73,32 @@ impl Drop for Api {
 }
 
 impl Api {
+    pub fn open_free_run(
+        &self,
+        wire_type: WireType,
+        address: &str,
+    ) -> Result<Handle<FreeRun>, OpenError> {
+        Handle::open(wire_type, address)
+    }
+
+    pub fn open_trigger_sync(
+        &self,
+        wire_type: WireType,
+        address: &str,
+    ) -> Result<Handle<TriggerSync>, OpenError> {
+        Handle::open(wire_type, address)
+    }
+
     pub fn open_trigger_asnyc(
         &self,
         wire_type: WireType,
         address: &str,
-    ) -> Result<Handle, OpenError> {
-        Handle::open(wire_type, address, ConnectionMode::TriggerAsync)
+    ) -> Result<Handle<TriggerAsync>, OpenError> {
+        Handle::open(wire_type, address)
     }
 }
 
-pub struct Handle(i32);
+pub struct Handle<T>(i32, PhantomData<fn() -> T>);
 pub enum WireType {
     /// USBTMC(YTUSB)
     UsbTmc,
@@ -87,10 +109,43 @@ pub enum WireType {
     /// HiSLIP
     HiSlip,
 }
-enum ConnectionMode {
-    Freerun,
-    Trigger,
-    TriggerAsync,
+pub mod connection_mode {
+    use sealed::sealed;
+
+    use dl950acqapi_sys::{SC_FREERUN, SC_TRIGGER, SC_TRIGGER_ASYNC};
+    use std::convert::Infallible;
+
+    pub struct FreeRun(Infallible);
+    /// Trigger mode (synchronized).
+    /// Triggers only after data transmission is confirmed.
+    pub struct TriggerSync(Infallible);
+    /// Trigger mode (not synchronized).
+    /// Triggers regardless of data transmission.
+    pub struct TriggerAsync(Infallible);
+
+    #[sealed]
+    pub trait ConnectionMode {
+        const MODE_NUM: u32;
+    }
+    #[sealed]
+    impl ConnectionMode for FreeRun {
+        const MODE_NUM: u32 = SC_FREERUN;
+    }
+    #[sealed]
+    impl ConnectionMode for TriggerSync {
+        const MODE_NUM: u32 = SC_TRIGGER;
+    }
+    #[sealed]
+    impl ConnectionMode for TriggerAsync {
+        const MODE_NUM: u32 = SC_TRIGGER_ASYNC;
+    }
+
+    #[sealed]
+    pub trait TriggerMode {}
+    #[sealed]
+    impl TriggerMode for TriggerSync {}
+    #[sealed]
+    impl TriggerMode for TriggerAsync {}
 }
 
 #[derive(Debug, Error)]
@@ -101,8 +156,8 @@ pub enum OpenError {
     ApiError(#[from] Error),
 }
 
-impl Handle {
-    fn open(wire_type: WireType, address: &str, mode: ConnectionMode) -> Result<Handle, OpenError> {
+impl<T: ConnectionMode> Handle<T> {
+    fn open(wire_type: WireType, address: &str) -> Result<Handle<T>, OpenError> {
         let wire = match wire_type {
             WireType::UsbTmc => SC_WIRE_USBTMC,
             WireType::VisaUsb => SC_WIRE_VISAUSB,
@@ -111,17 +166,14 @@ impl Handle {
         } as i32;
         let address = CString::new(address)?;
         let address = address.as_ptr() as _;
-        let mode = match mode {
-            ConnectionMode::Freerun => SC_FREERUN,
-            ConnectionMode::Trigger => SC_TRIGGER,
-            ConnectionMode::TriggerAsync => SC_TRIGGER_ASYNC,
-        } as i32;
+        let mode = T::MODE_NUM as i32;
         let mut handle = 0;
         let handle_ptr = &mut handle as *mut i32;
         check(unsafe { ScOpenInstrument(wire, address, mode, handle_ptr) })?;
-        Ok(Handle(handle))
+        Ok(Handle(handle, PhantomData))
     }
-
+}
+impl<T> Handle<T> {
     pub fn close(self) -> Result<(), Error> {
         self.close_impl()
     }
@@ -130,10 +182,117 @@ impl Handle {
         check(unsafe { ScCloseInstrument(self.0) })
     }
 }
-impl Drop for Handle {
+impl<T> Drop for Handle<T> {
     fn drop(&mut self) {
         if let Err(e) = self.close_impl() {
             e.report_drop_error();
         }
+    }
+}
+
+impl<T> Handle<T> {
+    pub fn start(&self) -> Result<(), Error> {
+        check(unsafe { ScStart(self.0) })
+    }
+
+    pub fn stop(&self) -> Result<(), Error> {
+        check(unsafe { ScStop(self.0) })
+    }
+
+    pub fn latch_data(&self) -> Result<(), Error> {
+        check(unsafe { ScLatchData(self.0) })
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct ChannelNumber {
+    pub channel: u8,
+    pub sub_channel: u8,
+}
+
+#[derive(Debug)]
+pub struct ReadTriggeredWaveformReturn {
+    pub received_len: usize,
+    pub completed: bool,
+    pub timestamp: NaiveDateTime,
+}
+
+impl<T: TriggerMode> Handle<T> {
+    pub fn read_triggered_waveform(
+        &self,
+        channel: ChannelNumber,
+        buffer: &mut [u8],
+    ) -> Result<ReadTriggeredWaveformReturn, Error> {
+        let mut received_len = 0;
+        let mut end_flag = 0;
+        let mut time_secs = 0u32;
+        let mut time_nanos = 0u32;
+        {
+            let buffer_ptr = buffer.as_mut_ptr() as *mut i8;
+            let buffer_len = buffer.len() as _;
+            let received_len = &mut received_len as *mut _;
+            let end_flag = &mut end_flag as *mut _;
+            let time_secs = &mut time_secs as *mut _;
+            let time_nanos = &mut time_nanos as *mut _;
+            check(unsafe {
+                ScGetAcqData(
+                    self.0,
+                    channel.channel as _,
+                    channel.sub_channel as _,
+                    buffer_ptr,
+                    buffer_len,
+                    received_len,
+                    end_flag,
+                    time_secs,
+                    time_nanos,
+                )
+            })?;
+        }
+        // We assume that the library is correct (nanoseconds part is never out of range).
+        let timestamp = DateTime::from_timestamp(time_secs as i64, time_nanos)
+            .expect("Nanosecond part is out of range")
+            .naive_utc(); // It seems that the timestamp is offset by 9:00, implying it's not true
+                          // UNIX time.
+        Ok(ReadTriggeredWaveformReturn {
+            received_len: received_len as _,
+            completed: end_flag != 0,
+            timestamp,
+        })
+    }
+
+    pub fn triggered_samples_len(&self, channel: ChannelNumber) -> Result<usize, Error> {
+        let mut ret = 0i64;
+        {
+            let ret = &mut ret as *mut _;
+            check(unsafe {
+                ScGetAcqDataLength(self.0, channel.channel as _, channel.sub_channel as _, ret)
+            })?;
+        }
+        // Returned value is between 0 and 999_999_999, so casting is safe.
+        Ok(ret as usize)
+    }
+
+    pub fn latched_acquisition_count(&self) -> Result<i64, Error> {
+        let mut ret = 0i64;
+        {
+            let ret = &mut ret as *mut _;
+            check(unsafe { ScGetLatchAcqCount(self.0, ret) })?;
+        }
+        // Returned value must be not very large... at least below the memory size.
+        Ok(ret)
+    }
+
+    pub fn acquisition_index(&self) -> Result<i64, Error> {
+        let mut ret = 0i64;
+        {
+            let ret = &mut ret as *mut _;
+            check(unsafe { ScGetAcqCount(self.0, ret) })?;
+        }
+        // Same as latched_acquisition_count
+        Ok(ret)
+    }
+
+    pub fn set_acquisition_index(&self, index: i64) -> Result<(), Error> {
+        check(unsafe { ScSetAcqCount(self.0, index) })
     }
 }
